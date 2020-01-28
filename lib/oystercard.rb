d056b6a8d72c2ada13cf77e87e6655101d3f138c
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-01-28 15:39:10 +0000

    Complete 08: touch in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,9 +2,11 @@ class Oystercard
     MAXIMUM_BALANCE = 90
     
     attr_reader :balance
-    
+    attr_accessor :in_journey
+
     def initialize
         @balance = 0
+        @in_journey = false
     end
 
     def top_up(amount)
@@ -14,5 +16,17 @@ class Oystercard
 
     def deduct(amount)
         @balance -= amount
+    end
+
+    def in_journey?
+        @in_journey
+    end
+
+    def touch_in
+        @in_journey = true
+    end
+
+    def touch_out
+        @in_journey = false
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -23,16 +23,58 @@ describe Oystercard do
     describe '#deduct' do
         it { is_expected.to respond_to(:deduct).with(1).argument }
         
-        # it "reduces the balance by a specified amount" do
-        #     maximum_balance = Oystercard::MAXIMUM_BALANCE
-        #     subject.top_up(maximum_balance)
-        #     subject.deduct(40)
-        #     expect(subject.balance).to eq(maximum_balance - 40)
-        # end
+        it "reduces the balance by a specified amount" do
+            maximum_balance = Oystercard::MAXIMUM_BALANCE
+            subject.top_up(maximum_balance)
+            subject.deduct(40)
+            expect(subject.balance).to eq(maximum_balance - 40)
+        end
 
         it 'deducts an amount from the balance' do
             subject.top_up(20)
             expect{ subject.deduct 3}.to change{ subject.balance }.by -3
         end
     end
+######################################
+
+    describe "#touch_in" do
+        it { is_expected.to respond_to(:touch_in).with(0).argument }
+
+        it 'changes in_journey to true' do
+            expect(subject.touch_in).to be true
+        end
+
+        it "can touch in" do
+            subject.touch_in
+            expect(subject).to be_in_journey
+        end
+    end
+
+    describe "#in_journey?" do
+      it { is_expected.to respond_to(:in_journey?).with(0).argument }
+
+      it 'returns false at the start' do
+        expect(subject.in_journey?).to eq false
+      end
+
+      it 'returns true when the user has touched in' do
+        subject.touch_in
+        expect(subject.in_journey?).to be true
+      end
+
+     it 'returns false when the user has touched out' do
+        subject.touch_out
+        expect(subject.in_journey?).to be false
+      end
+    end
+
+    describe "#touch_out" do
+      it { is_expected.to respond_to(:touch_out).with(0).argument }
+
+      it 'changes in_journey to false' do
+        subject.touch_in
+        expect(subject.touch_out).to be false
+      end
+    end
+    
 end
