76ed8cb091a9210afaa864a40d3e2394cdb1f2bc
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-01-30 10:49:47 +0000

    Complete 12: Add journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,13 @@ class Oystercard
     MAXIMUM_BALANCE = 90
     MINIMUM_FARE = 1
     
-    attr_reader :balance
+    attr_reader :balance, :journeys
     attr_accessor :entry_station
 
     def initialize
         @balance = 0
         @entry_station = nil
+        @journeys = []
     end
 
     def top_up(amount)
@@ -23,13 +24,17 @@ class Oystercard
         @entry_station ? true : false
     end
 
+    def create_journey(entry_station, exit_station)
+        journey = {"JID" => (@journeys.length + 1), "Entry" => entry_station, "Exit" => exit_station}
+    end
+
     def touch_in(station)
         raise "Balance not high enough for journey" if @balance < MINIMUM_FARE
         @entry_station = station
         in_journey?
     end
 
-    def touch_out
+    def touch_out(exit_station)
         @balance -= MINIMUM_FARE
         @entry_station = nil
         in_journey?
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,41 +1,45 @@
 require 'oystercard'
 
 describe Oystercard do
-    let(:station) {double("fake station")}
+  subject(:oyster) { described_class.new }
+  let(:station) {double("fake station")}
 
-    it "has a balance of 0 by default" do
-        expect(subject.balance).to eq 0
-    end
+  it "has a balance of 0 by default" do
+    expect(oyster.balance).to eq 0
+  end
+
+  it 'initializes an empty array' do
+    expect(oyster.journeys).to eq []
+  end
     
-    describe '#top_up' do
-    
-        it { is_expected.to respond_to(:top_up).with(1).argument }
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
 
-        it 'can top up the balance' do
-            expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-        end
-
-        it "raises an error if the maximum balance is exceeded" do
-            maximum_balance = Oystercard::MAXIMUM_BALANCE
-            subject.top_up maximum_balance
-            expect{ subject.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
-        end
+    it 'can top up the balance' do
+      expect{ oyster.top_up 1 }.to change{ oyster.balance }.by 1
     end
 
-    describe '#deduct' do
-        it { is_expected.to respond_to(:deduct).with(1).argument }
+    it "raises an error if the maximum balance is exceeded" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      oyster.top_up maximum_balance
+      expect{ oyster.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+      end
+    end
+
+  describe '#deduct' do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
         
-        it "reduces the balance by a specified amount" do
-            maximum_balance = Oystercard::MAXIMUM_BALANCE
-            subject.top_up(maximum_balance)
-            subject.deduct(40)
-            expect(subject.balance).to eq(maximum_balance - 40)
-        end
+    it "reduces the balance by a specified amount" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      oyster.top_up(maximum_balance)
+      oyster.deduct(40)
+      expect(oyster.balance).to eq(maximum_balance - 40)
+    end
 
-        it 'deducts an amount from the balance' do
-            subject.top_up(20)
-            expect{ subject.deduct 3}.to change{ subject.balance }.by -3
-        end
+    it 'deducts an amount from the balance' do
+      oyster.top_up(20)
+      expect{ oyster.deduct 3}.to change{ oyster.balance }.by -3
+      end
     end
 ######################################
 
@@ -44,87 +48,92 @@ describe Oystercard do
     end
 
     describe "#touch_in" do
-        before(:example) do
-            subject.top_up(2)
-            allow(station).to receive(:name) { "Aldgate East" }
-        end
+      before(:example) do
+        oyster.top_up(Oystercard::MAXIMUM_BALANCE)
+        allow(station).to receive(:name) { "Aldgate East" }
+      end
         
-        it { is_expected.to respond_to(:touch_in).with(1).argument }
+      it { is_expected.to respond_to(:touch_in).with(1).argument }
         
-        it 'updates an Oystercard to remember the entry station' do
-            expect { subject.touch_in(station.name) }.to change{ subject.entry_station }.to station.name 
-        end
+      it 'updates an Oystercard to remember the entry station' do
+        expect { oyster.touch_in(station.name) }.to change{ oyster.entry_station }.to station.name 
+      end
 
-        it 'changes in_journey to true' do
-            expect(subject.touch_in(station.name)).to be true
-        end
+      it 'changes in_journey to true' do
+        expect(oyster.touch_in(station.name)).to be true
+      end
 
-        it "can touch in" do
-            subject.touch_in(station.name)
-            expect(subject).to be_in_journey
-        end
+      it "can touch in" do
+        oyster.touch_in(station.name)
+        expect(oyster).to be_in_journey
+      end
     end
 
     describe "#touch_in" do
-        it 'requires minimum balance for a journey' do
-            expect{ subject.touch_in(station) }.to raise_error "Balance not high enough for journey"
-        end
+      it 'requires minimum balance for a journey' do
+        expect{ oyster.touch_in(station) }.to raise_error "Balance not high enough for journey"
+      end
     end
     
     describe "#in_journey?" do
       it { is_expected.to respond_to(:in_journey?).with(0).argument }
 
       it 'returns false at the start' do
-        expect(subject.in_journey?).to eq false
+        expect(oyster.in_journey?).to eq false
       end
 
       it 'returns true when the user has touched in' do
-        subject.top_up(2)
-        subject.touch_in(station)
-        expect(subject.in_journey?).to be true
+        oyster.top_up(Oystercard::MAXIMUM_BALANCE)
+        oyster.touch_in(station)
+        expect(oyster.in_journey?).to be true
       end
 
      it 'returns false when the user has touched out' do
-        subject.touch_out
-        expect(subject.in_journey?).to be false
+        allow(station).to receive(:name).and_return("Aldgate East", "Angel")
+        oyster.touch_out(station.name)
+        expect(oyster.in_journey?).to be false
       end
 
       it 'returns true when an entry station exists' do
-        subject.top_up(2)
+        oyster.top_up(Oystercard::MAXIMUM_BALANCE)
         allow(station).to receive(:name) { "Aldgate East" }
-        subject.touch_in(station.name)
-        expect(subject.in_journey?).to be true
+        oyster.touch_in(station.name)
+        expect(oyster.in_journey?).to be true
       end
       
       it 'returns false when an entry station does not exist' do
-        subject.top_up(2)
-        allow(station).to receive(:name) { "Aldgate East" }
-        subject.touch_in(station.name)
-        subject.touch_out
-        expect(subject.in_journey?).to be false
+        oyster.top_up(Oystercard::MAXIMUM_BALANCE)
+        allow(station).to receive(:name).and_return("Aldgate East", "Angel")
+        oyster.touch_in(station.name)
+        oyster.touch_out(station.name)
+        expect(oyster.in_journey?).to be false
       end
     end
 
     describe "#touch_out" do
-      it { is_expected.to respond_to(:touch_out).with(0).argument }
+      it { is_expected.to respond_to(:touch_out).with(1).argument }
       
       it 'changes entry station to nil' do
-        subject.top_up(2)
-        allow(station).to receive(:name) { "Aldgate East" }
-        subject.touch_in(station.name)
-        expect { subject.touch_out }.to change{ subject.entry_station}.to nil
+        allow(station).to receive(:name).and_return("Aldgate East", "Angel")
+        oyster.top_up(Oystercard::MAXIMUM_BALANCE)
+        oyster.touch_in(station.name)
+        oyster.touch_out(station.name)
+        expect(oyster.entry_station).to eq nil
       end
 
       it 'changes in_journey to false' do
-        subject.top_up(2)
-        subject.touch_in(station)
-        expect(subject.touch_out).to be false
+        oyster.top_up(Oystercard::MAXIMUM_BALANCE)
+        allow(station).to receive(:name).and_return("Aldgate East", "Angel")
+        oyster.touch_in(station.name)
+        oyster.touch_out(station.name)
+        expect(oyster).not_to be_in_journey
       end
  
-      it 'deducts the minimum fare from teh balance at touch out' do
-       subject.top_up(20)
-       subject.touch_in(station)
-       expect { subject.touch_out }.to change{ subject.balance }.by -described_class::MINIMUM_FARE
+      it 'deducts the minimum fare from the balance at touch out' do
+       oyster.top_up(Oystercard::MAXIMUM_BALANCE)
+       allow(station).to receive(:name).and_return("Aldgate East", "Angel")
+       oyster.touch_in(station.name)
+       expect { oyster.touch_out(station.name) }.to change{ oyster.balance }.by -(Oystercard::MINIMUM_FARE)
       end
     end
 
@@ -133,5 +142,16 @@ describe Oystercard do
             testclass = Oystercard.new
             expect(testclass.entry_station).to eq nil
         end
+    end
+
+    describe "#create_journey" do
+      it 'responds to create_journey' do
+        expect(oyster).to respond_to(:create_journey).with(2).arguments  
+      end  
+
+      it 'should create a hash with teh entry and exit journey' do
+        allow(station).to receive(:name).and_return("Aldgate East", "Angel")
+        expect(oyster.create_journey(station.name, station.name)).to eq({"JID"=>1, "Entry"=>"Aldgate East", "Exit"=>"Angel"})
+      end
     end
 end
